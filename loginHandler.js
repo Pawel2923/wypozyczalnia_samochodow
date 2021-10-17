41cff434f992b3fdb4c82d16200349a3a7f71ebd
Pawel2923 <83728661+Pawel2923@users.noreply.github.com>
2021-10-17 22:04:58 +0200

    dodano loginHandler.js
    Dodano skrypt do zmieniania ustawień w panelu administracyjnym oraz dodano loginHandler.js

---
+++ loginHandler.js
@@ -0,0 +1,57 @@
+const movePlaceholder = (name) => {
+    if (document.querySelector(`input[name="${name}"]`) != null) {
+        if (document.querySelector(`input[name="${name}"`).value.length == 0)
+            document.querySelector(`.${name} label`).classList.add('form-label-transform-start');
+        else 
+            document.querySelector(`.${name} label`).classList.remove('form-label-transform-start');  
+
+        document.querySelector(`form input[name="${name}"]`).addEventListener('focus',() => {
+            document.querySelector(`.${name} label`).classList.add('form-label-transform');
+        });
+        document.querySelector(`form input[name="${name}"]`).addEventListener('focusout',() => {
+            if (document.querySelector(`input[name="${name}"`).value == 0) {
+                document.querySelector(`.${name} label`).classList.remove('form-label-transform');
+                document.querySelector(`.${name} label`).classList.add('form-label-transform-end');
+            }
+        });
+    }
+};
+
+movePlaceholder('login');
+movePlaceholder('password');
+movePlaceholder('password-confirm');
+
+const passwdCheck = () => {
+    const passwd = document.querySelector('form input[name="password"]');
+    const passwdConfirm = document.querySelector('form input[name="password-confirm"]');
+
+    if (passwd != null) {
+        passwd.addEventListener('invalid', () => {
+            (passwd.value.length < 4) ? passwd.setCustomValidity("Hasło powinno mieć przynajmniej 4 znaki") : passwd.setCustomValidity("");
+        });
+        
+        passwd.addEventListener('valid', () => {
+            passwd.setCustomValidity("");
+        });
+    
+        if (passwdConfirm != null) {
+            passwdConfirm.addEventListener('keyup', () => {
+                (passwd.value != passwdConfirm.value) ? passwdConfirm.setCustomValidity("Hasła nie są zgodne") : passwdConfirm.setCustomValidity("");
+            });
+        }
+    }
+}
+
+const checkInput = (name) => {
+    if (name != null) {
+        name.addEventListener('invalid', () => {
+            name.classList.add('subscription-input-invalid2');
+        });
+        name.addEventListener('keyup', () => {
+            name.classList.remove('subscription-input-invalid2');
+        });
+    }
+};
+checkInput(document.querySelector('form input[name="login"]'));
+checkInput(document.querySelector('form input[name="password"]'));
+checkInput(document.querySelector('form input[name="password-confirm"]'));
