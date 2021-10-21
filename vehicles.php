4136d78d6f8a53bb111a284e098696f59a783ad7
Pawel2923 <83728661+Pawel2923@users.noreply.github.com>
2021-10-21 18:50:36 +0200

    Zaktualizowanie do najnowszej wersji
    Dodano vehicles.php i pomniejsze zmiany

--- vehicles.php
+++ vehicles.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="pl">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="author" content="Paweł Poremba">
@@ -12,37 +12,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles/main.css">
-    <link rel="stylesheet" href="styles/login.css">
     <script src="https://kit.fontawesome.com/32373b1277.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="back">
-        <i class="fas fa-times"></i>
-    </div>
-    <main>
-        <div class="header-wrapper">
-            <header>
-                <h1>Zmień swoje hasło</h1>
-            </header>
-        </div>
-        <div class="form-wrapper">
-            <form action="login.php" method="POST">
-                <div class="login">
-                    <label for="login-field">Adres E-mail</label>
-                    <br>
-                    <input type="email" name="login" id="login-field" required>
-                </div>
-                <div class="form-bottom">
-                    <button type="submit">Potwierdź</button>
-                </div>
-            </form>
-        </div>
-    </main>
-    <script>
-        document.querySelector('.back').addEventListener('click', () => {
-            window.location = './index.php';
-        }); 
-    </script>
-    <script src="loginHandler.js"></script>
+    
 </body>
 </html>
