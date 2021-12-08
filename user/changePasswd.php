0181e95213438a36b72f627fae623c5e45fd822a
Pawel2923 <83728661+Pawel2923@users.noreply.github.com>
2021-12-08 11:14:36 +0100

    Motyw panelu poprawka dołączenia kodu

--- user/changePasswd.php
+++ user/changePasswd.php
@@ -140,13 +140,11 @@
         }
     </style>
     <?php 
-        if (isset($_POST['theme']))
-        {
+        if (isset($_POST['theme'])) {
             if ($_POST['theme'] != "default")
                 echo '<link rel="stylesheet" href="styles/'.$_POST['theme'].'.css">';
         }
-        elseif (isset($_COOKIE['theme']))
-        {
+        elseif (isset($_COOKIE['theme'])) {
             if ($_COOKIE['theme'] != "default")
                 echo '<link rel="stylesheet" href="styles/'.$_COOKIE['theme'].'.css">';
         }
