771444e03b827e00ddc9d8d816e2b72fe37e8e76
Paweł Poremba <pawelporemba123@gmail.com>
2022-12-30 18:29:46 +0100

    Add shortcut icon

--- inc/logged-menu.php
+++ inc/logged-menu.php
@@ -6,7 +6,7 @@
                 echo '<li><a href="admin.php"><i class="fa-solid fa-hammer"></i> Panel administracyjny</a></li>';
         }
         ?>
-        <li><a href="user.php"><i class="fa-solid fa-gear"></i> Panel użytkownika</a></li>
+        <li><a href="user.php"><i class="fa-solid fa-user-gear"></i> Panel użytkownika</a></li>
         <li><a href="logout.php"><i class="fa-solid fa-right-from-bracket"></i> Wyloguj się</a></li>
     </ul>
 </div>
