89da755f2119d2445896eb8ffd61f50e86beee89
Paweł Poremba <pawelporemba123@gmail.com>
2023-01-06 15:53:54 +0100

    Fix message

--- inc/message.php
+++ inc/message.php
@@ -1,23 +1,34 @@
-<div class="message-wrapper">
-    <div class="overlay"></div>
-    <div class="message">
-        <div class="close"><i class="fas fa-times"></i></div>
-        <div class="msg">
+<?php
+if (isset($_SESSION['msg']) || isset($_SESSION['error'])) {
+?>
+    <div class="message-wrapper">
+        <div class="overlay"></div>
+        <div class="message">
+            <div class="close"><i class="fas fa-times"></i></div>
             <?php
             if (isset($_SESSION['msg'])) {
-                echo $_SESSION['msg'];
-                unset($_SESSION['msg']);
-            }
             ?>
-        </div>
-        <div class="error">
+                <div class="msg">
+                    <?php
+                    echo $_SESSION['msg'];
+                    unset($_SESSION['msg']);
+                    ?>
+                </div>
             <?php
+            }
             if (isset($_SESSION['error'])) {
-                echo $_SESSION['error'];
-                unset($_SESSION['error']);
+            ?>
+                <div class="error">
+                    <?php
+                    echo $_SESSION['error'];
+                    unset($_SESSION['error']);
+                    ?>
+                </div>
+            <?php
             }
             ?>
         </div>
     </div>
-</div>
-<?php if (isset($_SESSION['msg']) || isset($_SESSION['error'])) echo '<script src="../js/message.js"></script>' ?>
+    <script src="../js/message.js"></script>
+<?php
+}
